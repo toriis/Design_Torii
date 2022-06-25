32efda0e2c32c3f7cc9ff7ccddc6bf71ef376732
toriis <39819385+toriis@users.noreply.github.com>
2022-06-26 02:30:06 +0900

    第5回課題

--- cafe site/JavaScript/app.js
+++ cafe site/JavaScript/app.js
@@ -1,21 +1,39 @@
-//ページ内スクロールでゆっくり移動させる
+// スクロールでヘッダー部分の背景色を出現させる
+$(window).on('load scroll', function(){
+    $(function(){
+        var imgHeight = $('#js-mainVisual').outerHeight() - 50; //画像の高さを取得 位置調整のため-50する
+        var header = $('#js-header'); //ヘッダーコンテンツ
+
+            if ($(window).scrollTop() < imgHeight) {
+                //メインビジュアル内にいるので、クラスを外す
+                header.removeClass('headerColor-default');
+            }else {
+                //メインビジュアルより下までスクロールしたので、クラスを付けて色を変える
+                header.addClass('headerColor-default');
+            }
+    });
+});
+// END--スクロールでヘッダー部分の背景色を出現させる
+
+// ページ内スクロールでゆっくり移動させる
 $(function(){
     $('a[href^="#"]').click(function() {     /* リンク先が#から始まる時 */
-      var speed = 500;                     /* スクロールにかける時間を設定 */
-      var href = $(this).attr("href");     /* hrefの内容を取得 */
-      var target = $(href == "#" ? 'html' : href);  /* hrefが#だけだった場合、htmlに置き換えて、そうでない場合はhrefの値をtargetに格納 */
-      var position = target.offset().top;  /* 飛び先のIDの上からの位置を取得 */
+        var speed = 500;                     /* スクロールにかける時間を設定 */
+        var href = $(this).attr("href");     /* hrefの内容を取得 */
+        var target = $(href == "#" ? 'html' : href);  /* hrefが#だけだった場合、htmlに置き換えて、そうでない場合はhrefの値をtargetに格納 */
+        var position = target.offset().top - 50;  /* 飛び先のIDの上からの位置を取得 位置調整のため-50する*/
       $('body,html').animate({scrollTop:position}, speed, 'swing'); /* cssのアニメーションを使ってスクロールさせる */
         return false;
     });
 });
- //END--ページ内スクロールでゆっくり移動させる
+ // END--ページ内スクロールでゆっくり移動させる
 
+// ページトップボタン
 $(function() {
     var topBtn = $('#page_top');
     topBtn.hide();
 
-    //ボタンの表示設定
+    // ボタンの表示設定
     $(window).scroll(function() {
         if($(this).scrollTop() > 1000) {
         // 画面を1000pxスクロールしたら、ボタンを表示する
@@ -33,3 +51,13 @@ $(function() {
         return false;
     });
 });
+// END--ページトップボタン
+
+// ハンバーガーメニュー表示・非表示
+$(function() {
+    $('.btn').click(function() {
+        $('.sp_nav').fadeToggle();
+        $('.btn').toggleClass('open');
+    });
+});
+// END--ハンバーガーメニュー表示・非表示
